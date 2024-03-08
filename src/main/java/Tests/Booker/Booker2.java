9b48bb6958586c0cdd651e9eaa0bc0c3ea3dd97f
Lungelo_Shabangu <lungelo.shabangu@ayo4u.com>
2024-03-08 19:05:14 +0200

    update

--- src/main/java/Tests/Booker/Booker2.java
+++ src/main/java/Tests/Booker/Booker2.java
@@ -1,6 +1,7 @@
 package Tests.Booker;
 
 import CommonMethods.Steps;
+import ExtentReport.ExtentManager;
 import Utilities.ExcelDataReader;
 import Utilities.Global_Vars;
 import com.aventstack.extentreports.ExtentReports;
@@ -14,22 +15,25 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.io.File;
+import java.io.IOException;
 
 //import static CommonMethods.Steps.rightClickAndDragWithRobot;
 
 public class Booker2 {
 
     WebDriver driver;
-    protected ExtentReports extentReports;
-    protected ExtentTest extentTest;
+    private static ExtentReports extent;
+    protected ExtentTest test;
     Steps step;
 
     @BeforeTest
     public void setUpExtentReports() {
-        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(Global_Vars.ROOT_PATH + "/Reports");
-        extentReports = new ExtentReports();
-        extentReports.attachReporter(htmlReporter);
-        extentTest = extentReports.createTest("Booker");
+//        start reporters
+        extent = ExtentManager.createInstance("extent.html");
+        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("extent_report");
+//        create ExtentReports and attach report
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
     }
 
     @BeforeClass
@@ -43,15 +47,17 @@ public class Booker2 {
     }
 
     @Test(dataProvider = "testData")
-    public void yourTest(String FirstName, String subjectBooker, String EmailAddress ,String PhoneNumber, String commentBooker, String expectedOutput) throws InterruptedException {
+    public void yourTest(String FirstName, String subjectBooker, String EmailAddress ,String PhoneNumber, String commentBooker, String expectedOutput) throws InterruptedException, IOException {
+//        creates a toggle for the given test, adds all log events under it
+        test = extent.createTest("MyFirstTest", "Sample description");
 
         WebElement enterName=driver.findElement(By.xpath("//div//input[@id='name']"));
         step.scrollElementIntoView(driver,enterName);
         step.enterText(enterName, FirstName);
-
+        test.log(Status.INFO, "Adding First Name");
         WebElement enterEmailAddress=driver.findElement(By.xpath("//div//input[@placeHolder='Email']"));
         step.enterText(enterEmailAddress, EmailAddress);
-
+        test.log(Status.INFO, "Adding Email Address");
         WebElement enterSubject=driver.findElement(By.xpath("//div//input[@placeHolder='Subject']"));
         step.enterText(enterSubject, subjectBooker);
 
@@ -65,14 +71,20 @@ public class Booker2 {
         WebElement clickSubmitButton=driver.findElement(By.xpath("//button[contains(text(),'Submit')]"));
         step.clickElement(clickSubmitButton);
         Thread.sleep(6000);
+        test.log(Status.INFO, "Validating whether booking happened");
         WebElement output=driver.findElement(By.xpath("(//div//h2)[2]"));
         String actualOutput=output.getText();
+
         Assert.assertEquals(expectedOutput, actualOutput);
+        test.addScreenCaptureFromPath("screenshot.png");
 //        actualOutputcaptureScreenshot("Validate Booking");
     }
 
     @AfterClass
     public void tearDown() {
+
+        // calling flush writes everything to the log file
+        extent.flush();
         driver.quit();
     }
 
@@ -81,18 +93,5 @@ public class Booker2 {
         String filePath = Global_Vars.ROOT_PATH + "/TestData/TestData.xlsx";
         String sheetName = "Comment";
         return ExcelDataReader.readExcelData(filePath, sheetName);
-    }
-
-    public void captureScreenshot(String screenshotName) {
-        try {
-            TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
-            File screenshotFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
-            String destinationPath = "screenshots/" + screenshotName + ".png";
-            File destination = new File(destinationPath);
-            org.apache.commons.io.FileUtils.copyFile(screenshotFile, destination);
-            extentTest.addScreenCaptureFromPath(destinationPath);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
